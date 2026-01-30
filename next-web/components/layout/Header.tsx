33510bb3f5c1856637e9eef0276cd51e4d85be82
Subin <subinbs4a@gmail.com>
2026-01-30 10:56:54 +0530

    fixed the header layout for mobile UI

--- next-web/components/layout/Header.tsx
+++ next-web/components/layout/Header.tsx
@@ -9,11 +9,11 @@ export default function Header() {
       <Link href="/" className="block">
         <Logo />
       </Link>
-      <div className="flex justify-between items-start mt-1">
+      <div className="flex justify-between items-center mt-1">
         <p className="text-text-gray text-sm">Engineering your next 10x</p>
         <Link
           href="/login"
-          className="text-sm text-text-gray hover:bg-gray-100 hover:shadow-sm px-4 py-2 rounded-lg transition-all -mt-1"
+          className="text-sm text-text-gray hover:bg-gray-100 hover:shadow-sm px-4 py-2 -my-2 rounded-lg transition-all"
         >
           Investors Login
         </Link>
